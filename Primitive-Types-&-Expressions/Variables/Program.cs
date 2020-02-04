6afd3c8eec8ad1a7cbd7e5d39b37a11525fe7d62
VicMontanez <v_montanez@hotmail.com>
2020-02-04 18:09:16 -0500

    added different variable types

--- Primitive-Types-&-Expressions/Variables/Program.cs
+++ Primitive-Types-&-Expressions/Variables/Program.cs
@@ -7,6 +7,11 @@ namespace Variables
         static void Main(string[] args)
         {
             byte number = 2;
+            int count = 10;
+            float totalPrice = 28.85f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = true;
             Console.WriteLine(number);
         }
     }
