80f8f9fd5c7d5d2dfcbdbe87179b965a298a6ff2
VicMontanez <v_montanez@hotmail.com>
2020-02-04 19:38:04 -0500

    added convert to int32

---
+++ Primitive-Types-&-Expressions/TypeConversion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = "1234";
+            int i = Convert.ToInt32(number);
+            Console.WriteLine(i);
+
+        }
+    }
+}
