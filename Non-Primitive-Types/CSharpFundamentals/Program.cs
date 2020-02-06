13c6403c88192847176aceba9830f8ce93626f62
VicMontanez <v_montanez@hotmail.com>
2020-02-05 23:48:54 -0500

    string array

--- Non-Primitive-Types/CSharpFundamentals/Program.cs
+++ Non-Primitive-Types/CSharpFundamentals/Program.cs
@@ -14,7 +14,10 @@ namespace CSharpFundamentals
 
             var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
 
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(",", names);
 
+            Console.WriteLine(formattedNames);
         }
     }
 }
