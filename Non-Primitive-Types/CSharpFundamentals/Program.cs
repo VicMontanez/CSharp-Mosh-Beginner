7cca5351ceeab246891d5382d2aabe09dae41c8e
VicMontanez <v_montanez@hotmail.com>
2020-02-05 23:47:04 -0500

    strings

--- Non-Primitive-Types/CSharpFundamentals/Program.cs
+++ Non-Primitive-Types/CSharpFundamentals/Program.cs
@@ -7,21 +7,14 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var numbers = new int[3];
-            numbers[0] = 1;
+            var firstName = "Mosh";
+            var lastName = "Hamedani";
 
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[2]);
+            var fullName = firstName + " " + lastName;
 
-            var flags = new bool[3];
-            flags[0] = true;
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
 
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
 
-            var names = new string[3] { "Jack", "John", "Mary" };
         }
     }
 }
