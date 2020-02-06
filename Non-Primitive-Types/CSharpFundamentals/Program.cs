855e5d9c6c424e1747a15f868f50b2d79858983d
VicMontanez <v_montanez@hotmail.com>
2020-02-05 21:06:38 -0500

    arrays

--- Non-Primitive-Types/CSharpFundamentals/Program.cs
+++ Non-Primitive-Types/CSharpFundamentals/Program.cs
@@ -1,25 +1,18 @@
 ﻿using System;
 
+
 namespace CSharpFundamentals
 {
-    public class Person
-    {
-        public string FirstName;
-        public string LastName;
-
-        public void Introduce()
-        {
-            Console.WriteLine("My name is " + FirstName + " " + LastName);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.Introduce();
+            var numbers = new int[3];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
         }
     }
 }
