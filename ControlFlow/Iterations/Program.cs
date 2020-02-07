0d4884d26c7d83b153da21169aa8113259bed106
VicMontanez <v_montanez@hotmail.com>
2020-02-07 16:15:56 -0500

    foreach loop

--- ControlFlow/Iterations/Program.cs
+++ ControlFlow/Iterations/Program.cs
@@ -6,17 +6,16 @@ namespace Iterations
     {
         static void Main(string[] args)
         {
-            for (var i = 1; i <= 10; i++)
+            var name = "John Smith";
+
+            // for (var i = 0; i < name.Length; i++)
+            // {
+            //     Console.WriteLine(name[i]);
+            // }
+
+            foreach (var character in name)
             {
-                if (i%2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-            for (var i = 10; i >= 1; i--)
-            if (i%2 == 0)
-            {
-                Console.WriteLine(i);
+                Console.WriteLine(character);
             }
         }
     }
