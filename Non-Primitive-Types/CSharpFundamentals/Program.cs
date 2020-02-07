974f5f4978bf40391d3aaf15bd30ac0629d2b0e7
VicMontanez <v_montanez@hotmail.com>
2020-02-06 21:04:34 -0500

    convert string to enum

--- Non-Primitive-Types/CSharpFundamentals/Program.cs
+++ Non-Primitive-Types/CSharpFundamentals/Program.cs
@@ -19,6 +19,14 @@ namespace CSharpFundamentals
 
             var methodId = 3;
             Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)
+            Enum.Parse(typeof(ShippingMethod), methodName);
+
+            
         }
     }
 }
