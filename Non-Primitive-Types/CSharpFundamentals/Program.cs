96e858fcdfa0ff32d6cfb71204277f264a4bce13
VicMontanez <v_montanez@hotmail.com>
2020-02-06 20:41:07 -0500

    enums

--- Non-Primitive-Types/CSharpFundamentals/Program.cs
+++ Non-Primitive-Types/CSharpFundamentals/Program.cs
@@ -3,26 +3,19 @@
 
 namespace CSharpFundamentals
 {
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express =3
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var firstName = "Mosh";
-            var lastName = "Hamedani";
-
-            var fullName = firstName + " " + lastName;
-
-            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
-
-            var names = new string[3] { "John", "Jack", "Mary" };
-            var formattedNames = string.Join(",", names);
-
-            Console.WriteLine(formattedNames);
-
-            var text = @"Hi John
-            Look into the following paths
-            c:\folder1\folder2\folder3\folder4";
-            Console.WriteLine(text);
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
         }
     }
 }
