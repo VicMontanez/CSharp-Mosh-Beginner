4519c129c82515b7bc6c6e6405198f901a55a5b8
VicMontanez <v_montanez@hotmail.com>
2020-02-11 18:38:40 -0500

    checked length of array

--- ArrayAndList/CSharpFundamentals/Program.cs
+++ ArrayAndList/CSharpFundamentals/Program.cs
@@ -7,6 +7,10 @@ namespace CSharpFundamentals
         static void Main(string[] args)
         {
             var numbers = new[] { 3, 7, 9, 2, 14, 8 };
+
+            //Length
+            Console.WriteLine("Length " + numbers.Length);
+
         }
     }
 }
