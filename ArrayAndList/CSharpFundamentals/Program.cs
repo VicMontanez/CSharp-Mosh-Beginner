68da495bfda35b460c542b47379bc338af5b79fc
VicMontanez <v_montanez@hotmail.com>
2020-02-11 20:25:10 -0500

    reverse method

--- ArrayAndList/CSharpFundamentals/Program.cs
+++ ArrayAndList/CSharpFundamentals/Program.cs
@@ -34,10 +34,18 @@ namespace CSharpFundamentals
             //Sort ()
             Array.Sort(numbers);
 
+            Console.WriteLine
             ("Effect of Sort()");
             foreach (var n in numbers)
                 Console.WriteLine(n);
 
+            //Reverse()
+            Array.Reverse(numbers);
+
+            Console.WriteLine
+            ("Effect of Reverse()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
         }
     }
 }
