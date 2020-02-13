addbd2efd645f1a774c2d76d739617b14a97a3b6
VicMontanez <v_montanez@hotmail.com>
2020-02-12 22:42:58 -0500

    time span

--- Working-With-Dates/DateNTime/Program.cs
+++ Working-With-Dates/DateNTime/Program.cs
@@ -6,21 +6,16 @@ namespace DateNTime
     {
         static void Main(string[] args)
         {
+          var timeSpan = new TimeSpan(1, 2, 3);
           
-          var dateTime = new DateTime(2015, 1, 1);
-          var now = DateTime.Now;
-          var today = DateTime.Today;
+          var timeSpan1 = new TimeSpan(1, 0, 0);
+          var timeSpan2 = TimeSpan.FromHours(1);
 
-        //   Console.WriteLine("Hour: " + now.Hour);
-        //   Console.WriteLine("Minute: " + now.Minute);
+          var start = DateTime.Now;
+          var end = DateTime.Now.AddMinutes(2);
+          var duration = end - start;
 
-          var tomorrow = now.AddDays(1);
-          var yesterday = now.AddDays(-1);
-
-        //   Console.WriteLine(now.ToLongDateString());
-        //  Console.WriteLine(now.ToShortDateString());
-        //   Console.WriteLine(now.ToLongTimeString());
-           Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+          Console.WriteLine("Duration: " + duration);
         }
     }
 }
