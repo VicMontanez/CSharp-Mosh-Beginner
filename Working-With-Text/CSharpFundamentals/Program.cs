d9712eb0e879ab78080df6c688d8830ea2f159ab
VicMontanez <v_montanez@hotmail.com>
2020-02-13 23:40:34 -0500

    trim method

---
+++ Working-With-Text/CSharpFundamentals/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Victor Montanez ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+        }
+    }
+}
