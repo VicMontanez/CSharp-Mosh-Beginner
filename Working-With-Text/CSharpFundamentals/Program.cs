f70463e9569f1e26a9669ebeb4a96cc4f9a095de
VicMontanez <v_montanez@hotmail.com>
2020-02-13 23:44:45 -0500

    split first and last name

--- Working-With-Text/CSharpFundamentals/Program.cs
+++ Working-With-Text/CSharpFundamentals/Program.cs
@@ -10,6 +10,12 @@ namespace CSharpFundamentals
             Console.WriteLine("Trim: '{0}'", fullName.Trim);
             
             Console.WriteLine("ToUpper: '{0}'", fullName.Trim.ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
         }
     }
 }
