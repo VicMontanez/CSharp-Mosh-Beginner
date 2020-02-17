92c07b37cb9f758e06d08ade39e4c4902128550e
VicMontanez <v_montanez@hotmail.com>
2020-02-17 17:28:18 -0500

    made new class

--- Working-With-Text/CSharpFundamentals/Program.cs
+++ Working-With-Text/CSharpFundamentals/Program.cs
@@ -8,33 +8,13 @@ namespace CSharpFundamentals
         {
             var sentence = "This is going to be a really really long text.";
 
-            var summary = SummarizeText(sentence);
+            var summary = StringUtility.SummarizeText(sentence, 25);
             Console.WriteLine(summary);
 
 
 
         }
-        static string SummarizeText(string text, int maxLength = 20)
-        {
-
-            if (text.Length < maxLength)
-                return text;
-
-            var words = text.Split(' ');
-            var totalCharacters = 0;
-            var summaryWords = new List<string>();
-
-            foreach (var word in words)
-            {
-                summaryWords.Add(word);
-
-                totalCharacters += word.Length + 1;
-                if (totalCharacters > maxLength)
-                    break;
-            }
-
-            return String.Join(" ", summaryWords) + "...";
-        }
-
-
     }
+
+
+}
