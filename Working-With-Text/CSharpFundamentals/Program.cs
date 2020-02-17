c5281f76425d31a0d3862b7cbb54721bded917bc
VicMontanez <v_montanez@hotmail.com>
2020-02-17 17:19:15 -0500

    cleaned up code

--- Working-With-Text/CSharpFundamentals/Program.cs
+++ Working-With-Text/CSharpFundamentals/Program.cs
@@ -15,26 +15,25 @@ namespace CSharpFundamentals
         {
             const int maxLength = 20;
 
-            if (sentence.Length < maxLength)
-                Console.WriteLine(sentence);
-            else
+            if (text.Length < maxLength)
+                return text;
+
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
             {
-                var words = sentence.Split(' ');
-                var totalCharacters = 0;
-                var summaryWords = new List<string>();
+                summaryWords.Add(word);
 
-                foreach (var word in words)
-                {
-                    summaryWords.Add(word);
-
-                    totalCharacters += word.Length + 1;
-                    if (totalCharacters > maxLength)
-                        break;
-                }
-
-                var summary = String.Join(" ", summaryWords) + "...";
-                Console.WriteLine(summary);
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                    break;
             }
 
+            var summary = String.Join(" ", summaryWords) + "...";
+            return summary;
         }
+
+
     }
