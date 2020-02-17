729372090985f3e92e2cd7ae1224cbdc517a4ade
VicMontanez <v_montanez@hotmail.com>
2020-02-17 17:06:34 -0500

    string join

--- Working-With-Text/CSharpFundamentals/Program.cs
+++ Working-With-Text/CSharpFundamentals/Program.cs
@@ -6,32 +6,30 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var fullName = "Victor Montanez ";
-            Console.WriteLine("Trim: '{0}'", fullName.Trim);
-            
-            Console.WriteLine("ToUpper: '{0}'", fullName.Trim.ToUpper());
+            var sentence = "This is going to be a really really long text.";
+            const int maxLength = 20;
 
-            var index = fullName.IndexOf(' ');
-            var firstName = fullName.Substring(0, index);
-            var lastName = fullName.Substring(index + 1);
-            Console.WriteLine("FirstName: " + firstName);
-            Console.WriteLine("LastName: " + lastName);
+            if (sentence.Length < maxLength)
+                Console.WriteLine(sentence);
+            else
+            {
+                var words = sentence.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
 
-            var names = fullName.Split(' ');
-            Console.WriteLine("FirstName: " + names[0]);
-            Console.WriteLine("LastName: " + names[1]);
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
 
-            Console.WriteLine(fullName.Replace("Montanez", "M"));
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > maxLength)
+                        break;
+                }
 
-            if(String.IsNullOrWhiteSpace(" "))
-            Console.WriteLine("Invalid");
+                var summary = String.Join(" ", summaryWords) + "...";
+                Console.WriteLine(summary);
 
-            var str = "25";
-            var age = Convert.ToByte(str);
-            Console.WriteLine(age);
-
-            float price = 29.95f;
-            Console.WriteLine(price.ToString("C0"))
+            }
         }
     }
 }
