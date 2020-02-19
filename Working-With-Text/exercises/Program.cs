d6b655ac0912403b151f063e9a7d79f45384aef4
VicMontanez <v_montanez@hotmail.com>
2020-02-18 20:21:53 -0500

    exercise for reversing name

---
+++ Working-With-Text/exercises/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? ");
+            var name = Console.ReadLine();
+
+            var reversed = ReversedName(name);
+            Console.WriteLine("Reversed name: " + reversed);
+
+
+            Console.WriteLine("Reversed name: " + reversed);
+        }
+
+        public static string ReversedName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+
+            return new string(array);
+
+            return reversed;
+        }
+    }
+}
