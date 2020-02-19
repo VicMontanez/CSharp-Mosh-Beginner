bcaf474c515f1daab56adf3fc6e1d943d1163fcd
VicMontanez <v_montanez@hotmail.com>
2020-02-18 19:39:59 -0500

    starter sting

--- Working-With-Text/CSharpFundamentals/Program.cs
+++ Working-With-Text/CSharpFundamentals/Program.cs
@@ -7,7 +7,8 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var builder = new StringBuilder();
+            var builder = new StringBuilder("Hello World");
+
             builder.Append('-', 10);
             builder.AppendLine();
             builder.Append("Header");
