2bbcda652e783ddbee0fb071d9760be15667bb32
VicMontanez <v_montanez@hotmail.com>
2020-02-18 19:45:54 -0500

    cleaned code by chaining methods

--- Working-With-Text/CSharpFundamentals/Program.cs
+++ Working-With-Text/CSharpFundamentals/Program.cs
@@ -9,14 +9,15 @@ namespace CSharpFundamentals
         {
             var builder = new StringBuilder("Hello World");
 
-            builder.Append('-', 10);
-            builder.AppendLine();
-            builder.Append("Header");
-            builder.Append('-', 10);
+            builder.Append('-', 10)
+            .AppendLine()
+            .Append("Header")
+            .Append('-', 10)
 
-            builder.Replace('-', '+');
-            builder.Remove(0, 10);
-            builder.Insert(0, new string('-', 10));
+            .Replace('-', '+')
+            .Remove(0, 10)
+            .Insert(0, new string('-', 10));
+
             Console.WriteLine(builder);
 
             Console.WriteLine("First Char: " + builder[0]);
