6af25cd8e94295f56dd8062d6487ff41d456b44c
VicMontanez <v_montanez@hotmail.com>
2020-02-19 21:18:54 -0500

    file copy

---
+++ Working-With-Files/FileFundamentals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace FileFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+        }
+    }
+}
