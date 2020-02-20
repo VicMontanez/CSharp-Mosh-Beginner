ad8c45ecccc3ffe28efb11ea4fcfc420166acf42
VicMontanez <v_montanez@hotmail.com>
2020-02-20 18:58:51 -0500

    created files variable

---
+++ Working-With-Files/FileFundamentals/directories.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            var files = Directory.GetFiles(@"c:\projects\CSharpFundamentals", "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+                Console.WriteLine(file);
+
+
+        }
+    }
+}
